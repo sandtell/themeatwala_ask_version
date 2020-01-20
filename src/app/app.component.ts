f323ba546fe67780f7cf684323c2a5d493f0e514
sandtell <sandtellcustomercare@gmail.com>
2020-01-20 16:27:24 +0530

    slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,11 @@ export class MyApp {
 
   pages: Array<{title: string, component: any, icon: string,}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+              public platform: Platform,
+              public statusBar: StatusBar,
+              public splashScreen: SplashScreen,
+              ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -36,9 +40,15 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
+  }
+
+
+  loginPage(){
+    this.nav.push('LoginPage');
+    // this.navCtrl.push('LoginPage');
   }
 
   openPage(page) {
